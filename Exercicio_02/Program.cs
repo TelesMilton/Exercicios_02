f963b6b279c7d942734088755ae0f304e96a81aa
Milton <telesmilton1000@gmail.com>
2021-07-31 03:23:24 -0300

    subindo arquivos

---
+++ Exercicio_02/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double x, y, d;
+            Console.WriteLine(" Digite o valor de x : ");
+            x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(" Digite o valor de y : ");
+            y = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(" Digite o valor de d : ");
+            d = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Resultado = " + (x + y + d + (5 * 77) + 8 - (10 * (8 / 3))));
+            Console.ReadKey();
+        }
+    }
+}
