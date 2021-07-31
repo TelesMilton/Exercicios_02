2dbd29c1c932c16df4513e86d10c33572f722cdf
Milton <telesmilton1000@gmail.com>
2021-07-31 03:57:10 -0300

    set arquivo

--- Exercicio_02/Program.cs
+++ Exercicio_02/Program.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+/* 2) dado a seguinte expressão:
+ * x + y + d + 5 * 77 + 8 -10 *( 8/3)
+ * Qual é o resultado da operação feita em algoritmo
+ */
 
 namespace Exercicio_02
 {
@@ -11,15 +15,15 @@ namespace Exercicio_02
         static void Main(string[] args)
         {
 
-            double x, y, d;
+            double x, y, d, resultado, parcial;
             Console.WriteLine(" Digite o valor de x : ");
             x = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine(" Digite o valor de y : ");
             y = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine(" Digite o valor de d : ");
             d = Convert.ToDouble(Console.ReadLine());
-
-            Console.WriteLine("Resultado = " + (x + y + d + (5 * 77) + 8 - (10 * (8 / 3))));
+           
+            Console.WriteLine("Resultado = " + (x + y + d + (5 * 77) + 8 - (10 *(double)8/(double)3)));
             Console.ReadKey();
         }
     }
